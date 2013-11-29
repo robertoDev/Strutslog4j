0658238223d5303ca62941703238436520e3c5a2
Roberto Girelli <roberto.gireli@gmail.com>
2013-11-29 10:55:25 +0100

    first commit

---
+++ src/it/blackcat/springlog4j/HelloWorld.java
@@ -0,0 +1,20 @@
+package it.blackcat.springlog4j;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: PGB
+ * Date: 29/11/13
+ * Time: 9.51
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorld {
+    private String message;
+
+    public void setMessage(String message){
+        this.message  = message;
+    }
+
+    public void getMessage(){
+        System.out.println("Your Message : " + message);
+    }
+}
---
+++ src/it/blackcat/springlog4j/MainApp.java
@@ -0,0 +1,30 @@
+package it.blackcat.springlog4j;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: PGB
+ * Date: 29/11/13
+ * Time: 9.52
+ * To change this template use File | Settings | File Templates.
+ */
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.apache.log4j.Logger;
+
+public class MainApp {
+
+    static Logger log = Logger.getLogger(MainApp.class.getName());
+
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("beans.xml");
+
+        log.info("Going to create HelloWord Obj");
+
+        HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+
+        obj.getMessage();
+
+        log.info("Exiting the program");
+    }
+}
